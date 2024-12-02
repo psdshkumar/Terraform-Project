9aab06397cfffd9c8cb8fd9fcb08c33239460df2
psdshkumar <146923272+psdshkumar@users.noreply.github.com>
2024-12-02 12:09:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,45 +1,11 @@
-provider "aws" {
-  region = "ap-south-1" # Replace with your desired AWS region
-}
 
-# Security Group to allow SSH traffic
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# EC2 Instance resource
-resource "aws_instance" "website" {
-  ami           = "ami-0dee22c13ea7a9a67" # Replace with your AMI ID
-  instance_type = "t2.micro"
-  key_name      = "terraform-key"         # Replace with your key pair name
-  security_groups = [aws_security_group.allow_ssh.name]
-
-  
-provisioner "file" {
-  source      = "C:/Users/psdsh/Terraform/python.py"  # Windows path with forward slashes
-  destination = "/home/ubuntu/app.py"  # Path on the EC2 instance where the file will be copied
-}
-
-  provisioner "remote-exec" {
+ provisioner "remote-exec" {
     inline = [
       "echo 'Hello from the remote instance'",
       "sudo apt update -y",
-      "sudo apt install python3 -y",  # Added missing comma after this line
+      "sudo apt install python3 -y",  # Fixed missing comma after this line
+      "sudo apt install apache2 -y",
+      "sudo apt install libapache2-mod-wsgi-py3 -y",
       "sudo apt-get install -y python3-pip",
       "cd /home/ubuntu",
       "sudo pip3 install flask",
@@ -48,7 +14,7 @@ provisioner "file" {
 
     connection {
       type        = "ssh"
-      user        = "ubuntu"                              # Default username for Ubuntu AMIs
+      user        = "ubuntu"  # Default username for Ubuntu AMIs
       private_key = file("C:/Users/psdsh/Downloads/terraform-key.pem")  # Path to your private key
       host        = self.public_ip
     }
@@ -63,4 +29,3 @@ provisioner "file" {
 output "instance_public_ip" {
   value = aws_instance.website.public_ip
 }
-
